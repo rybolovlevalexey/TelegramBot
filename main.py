5cc9cdb951267dc70d83597a486bd6ecbf0badc5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-04-01 14:04:00 +0300

    бот принимает updates и отправляет сообщения

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import dataclasses
 import requests
 
-url = "http://numbersapi.com/"
-res = requests.get(url + "43" + "/trivia")
-print(res.status_code)
+
+@dataclasses.dataclass
+class AboutBot:
+    token: str
+
+
+file = open("service information.txt")
+bot = AboutBot(file.readline().split()[1])
+chat_id = "728208900"
+url = f"https://api.telegram.org/bot{bot.token}/getUpdates"
+res = requests.get(url)
+#res1 = requests.get(url, params={"chat_id": chat_id, "text": "и тебе не хворать"})
 print(res.text)
