de8d4a91b38b4d664f2a6911440dc421450a4ccf
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-04-01 18:19:25 +0300

    эхо-бот с методами /start и /help

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import dataclasses
-import requests
-
-
-@dataclasses.dataclass
-class AboutBot:
-    token: str
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
 
 
 file = open("service information.txt")
-bot = AboutBot(file.readline().split()[1])
-chat_id = "728208900"
-url = f"https://api.telegram.org/bot{bot.token}/getUpdates"
-res = requests.get(url)
-#res1 = requests.get(url, params={"chat_id": chat_id, "text": "и тебе не хворать"})
-print(res.text)
+bot = Bot(token=file.readline().split()[1])
+dp: Dispatcher = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
