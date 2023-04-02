c2caca1a9c31f8fe3220d0b0bdaf874492604ed9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-04-02 15:52:13 +0300

    бот в ответ может отправлять фото, все обработчики указываются не через декораторы, а через добавление в диспетчер

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, ContentType
 from aiogram.filters import Command
 
 
@@ -7,19 +7,24 @@ file = open("service information.txt")
 bot = Bot(token=file.readline().split()[1])
 dp: Dispatcher = Dispatcher()
 
-@dp.message(Command(commands=["start"]))
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-@dp.message()
 async def send_echo(message: Message):
     await message.answer(message.text)
 
 
+# в диспетчере указывается, на какие сообщения, как реагировать
+dp.message.register(process_start_command, Command(commands=['start']))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_echo)  # в самом конце, потому что ловит абсолютно любые сообщения
 if __name__ == "__main__":
     dp.run_polling(bot)
